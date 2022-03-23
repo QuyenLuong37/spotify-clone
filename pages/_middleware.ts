2154be8c79faffe5945251056bb5eec2c489603b
Anh quyen <ducquyenluong97@gmail.com>
2022-03-23 17:17:25 +0700

    up

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -8,14 +8,17 @@ export async function middleware(req: any) {
   const token = await getToken({
     req,
     secret: process.env.SPOTIFY_CLIENT_SECRET,
+
   })
   // 
   const { pathname } = req.nextUrl
-  return NextResponse.next()
-  if (pathname.includes('/api/auth') || token) {
-    }
-    
-  if (!token || pathname !== '/login') {
+  if (pathname.includes('/api/auth')) {
+    return NextResponse.next()
+  }
+
+  if (token) {
+    return NextResponse.next()
+  } else {
     const url = req.nextUrl.clone()
     url.pathname = '/login'
     return NextResponse.rewrite(url)
