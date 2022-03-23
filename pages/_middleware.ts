f7b12e75fc4b341258e3442f2a1a98eb28c635af
Anh quyen <ducquyenluong97@gmail.com>
2022-03-23 14:28:44 +0700

    eo biet tai sao lai ko redirect ve dc

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,20 +4,20 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 
 export async function middleware(req: any) {
-  // const token = await getToken({
-  //   req,
-  //   secret: process.env.SPOTIFY_CLIENT_SECRET,
-  // })
-  // // 
-  // const { pathname } = req.nextUrl
-  // if (pathname.includes('/api/auth') || token) {
-  //     return NextResponse.next()
-  //   }
+  const token = await getToken({
+    req,
+    secret: process.env.SPOTIFY_CLIENT_SECRET,
+  })
+  // 
+  const { pathname } = req.nextUrl
+  if (pathname.includes('/api/auth') || token) {
+      return NextResponse.next()
+    }
     
-  // if (!token) {
-  //   const url = req.nextUrl.clone()
-  //   url.pathname = '/seach'
-  //   return NextResponse.rewrite(url)
-  // }
+  if (!token) {
+    const url = req.nextUrl.clone()
+    url.pathname = '/login'
+    return NextResponse.rewrite(url)
+  }
   return NextResponse.next()
 }
