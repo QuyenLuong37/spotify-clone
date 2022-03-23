d4c78e1c8b23ca6ece53e8d96f1760028a524885
Anh quyen <ducquyenluong97@gmail.com>
2022-03-23 16:06:57 +0700

    up

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,6 +4,7 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 
 export async function middleware(req: any) {
+  
   const token = await getToken({
     req,
     secret: process.env.SPOTIFY_CLIENT_SECRET,
@@ -14,10 +15,9 @@ export async function middleware(req: any) {
       return NextResponse.next()
     }
     
-  if (!token) {
+  if (!token || pathname !== '/login') {
     const url = req.nextUrl.clone()
     url.pathname = '/login'
     return NextResponse.rewrite(url)
   }
-  return NextResponse.next()
 }
